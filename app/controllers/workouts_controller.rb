98e31839611ac1054e05c335b0b20f9103ff1d4e
Sophia Bell <sophbell87@gmail.com>
2020-05-18 12:38:32 +0100

    refactoring partial, adding more buttons

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -3,14 +3,9 @@
 class WorkoutsController < ApplicationController
   include WorkoutsHelper
 
-  # def index 
-  #   @workouts
-  # end
-
   def show
     @workout_id = params[:id]
     videos = api_call(@workout_id)
     @workouts = pull_video_info(videos)
-    # redirect_to workouts_path(id: @workout_id)
   end
 end
