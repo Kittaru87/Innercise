b0080c61655ab33305eb0027a08e3942e01a421a
Sophia Bell <sophbell87@gmail.com>
2020-05-18 12:47:38 +0100

    feature tests for all buttons

--- spec/features/view_playlist_spec.rb
+++ spec/features/view_playlist_spec.rb
@@ -9,6 +9,43 @@ RSpec.feature 'Pull up list of videos', type: :feature do
 
   it 'can navigate to the correct workout page' do
     click_link 'Arms'
-    expect(page).to have_content('Arm workouts')
+    expect(page).to have_content('arms workouts')
+    expect{page}.to_not raise_error
+  end
+
+  it 'can navigate to the correct workout page' do
+    click_link 'Shoulders'
+    expect(page).to have_content('shoulders workouts')
+    expect{page}.to_not raise_error
+  end
+
+  it 'can navigate to the correct workout page' do
+    click_link 'Back'
+    expect(page).to have_content('back workouts')
+    expect{page}.to_not raise_error
+  end
+
+  it 'can navigate to the correct workout page' do
+    click_link 'Chest'
+    expect(page).to have_content('chest workouts')
+    expect{page}.to_not raise_error
+  end
+
+  it 'can navigate to the correct workout page' do
+    click_link 'Glutes'
+    expect(page).to have_content('glutes workouts')
+    expect{page}.to_not raise_error
+  end
+
+  it 'can navigate to the correct workout page' do
+    click_link 'Legs'
+    expect(page).to have_content('legs workouts')
+    expect{page}.to_not raise_error
+  end
+
+  it 'can navigate to the correct workout page' do
+    click_link 'Core'
+    expect(page).to have_content('core workouts')
+    expect{page}.to_not raise_error
   end
 end
