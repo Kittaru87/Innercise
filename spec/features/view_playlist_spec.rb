3ece482100e53f784b2e45ab066855d5c509e830
Sophia Bell <sophbell87@gmail.com>
2020-05-22 20:19:54 +0100

    adding feature tests again

--- spec/features/view_playlist_spec.rb
+++ spec/features/view_playlist_spec.rb
@@ -1,57 +1,57 @@
 # frozen_string_literal: true
 
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.feature 'Pull up list of videos', type: :feature do
-#   before(:each) do
-#     visit '/'
-#   end
+RSpec.feature 'Pull up list of videos', type: :feature do
+  before(:each) do
+    visit '/'
+  end
 
-#   it 'can navigate to the correct workout page' do
-#     click_link 'ARMS'
-#     expect(page).to have_content('Arms workouts')
-#     expect { page }.to_not raise_error
-#   end
+  it 'can navigate to the correct workout page' do
+    click_link 'ARMS'
+    expect(page).to have_content('Arms workouts')
+    expect { page }.to_not raise_error
+  end
 
-#   it 'can navigate to the correct workout page' do
-#     click_link 'SHOULDERS'
-#     expect(page).to have_content('Shoulders workouts')
-#     expect { page }.to_not raise_error
-#   end
+  it 'can navigate to the correct workout page' do
+    click_link 'SHOULDERS'
+    expect(page).to have_content('Shoulders workouts')
+    expect { page }.to_not raise_error
+  end
 
-#   it 'can navigate to the correct workout page' do
-#     click_link 'BACK'
-#     expect(page).to have_content('Back workouts')
-#     expect { page }.to_not raise_error
-#   end
+  it 'can navigate to the correct workout page' do
+    click_link 'BACK'
+    expect(page).to have_content('Back workouts')
+    expect { page }.to_not raise_error
+  end
 
-#   it 'can navigate to the correct workout page' do
-#     click_link 'CHEST'
-#     expect(page).to have_content('Chest workouts')
-#     expect { page }.to_not raise_error
-#   end
+  it 'can navigate to the correct workout page' do
+    click_link 'CHEST'
+    expect(page).to have_content('Chest workouts')
+    expect { page }.to_not raise_error
+  end
 
-#   it 'can navigate to the correct workout page' do
-#     click_link 'GLUTES'
-#     expect(page).to have_content('Glutes workouts')
-#     expect { page }.to_not raise_error
-#   end
+  it 'can navigate to the correct workout page' do
+    click_link 'GLUTES'
+    expect(page).to have_content('Glutes workouts')
+    expect { page }.to_not raise_error
+  end
 
-#   it 'can navigate to the correct workout page' do
-#     click_link 'LEGS'
-#     expect(page).to have_content('Legs workouts')
-#     expect { page }.to_not raise_error
-#   end
+  it 'can navigate to the correct workout page' do
+    click_link 'LEGS'
+    expect(page).to have_content('Legs workouts')
+    expect { page }.to_not raise_error
+  end
 
-#   it 'can navigate to the correct workout page' do
-#     click_link 'CORE'
-#     expect(page).to have_content('Core workouts')
-#     expect { page }.to_not raise_error
-#   end
+  it 'can navigate to the correct workout page' do
+    click_link 'CORE'
+    expect(page).to have_content('Core workouts')
+    expect { page }.to_not raise_error
+  end
 
-#   it 'can navigate to the correct workout page' do
-#     click_link 'YOGA'
-#     expect(page).to have_content('Yoga workouts')
-#     expect { page }.to_not raise_error
-#   end
-# end
+  it 'can navigate to the correct workout page' do
+    click_link 'YOGA'
+    expect(page).to have_content('Yoga workouts')
+    expect { page }.to_not raise_error
+  end
+end
