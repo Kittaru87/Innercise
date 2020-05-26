9835a4a72f18c7fc844a54aad402f45a04dbea6d
Sophia Bell <sophbell87@gmail.com>
2020-05-26 13:50:55 +0100

    refactored controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -4,10 +4,9 @@ class WorkoutsController < ApplicationController
   include WorkoutsHelper
 
   def show
-    @workout_id = params[:id]
-    @next_page = params[:nextPageToken]
-    videos = api_call(@workout_id)
-    @workouts = pull_video_info(videos)
+  @workout_id = params[:id]
+  # @next_page = params[:nextPageToken]
+  retrieve_videos(@workout_id, "")
   end
 
   def update
@@ -15,34 +14,16 @@ class WorkoutsController < ApplicationController
     puts @next_page
     @workout_id = params[:id]
     puts @workout_id
-    videos = api_call(@workout_id, @next_page)
-    @workouts = pull_video_info(videos)
+    retrieve_videos(@workout_id, @next_page)
     puts @workouts
     redirect_to workout_path(@workout_id)
   end
+
+private 
+
+  def retrieve_videos(workout_id, next_page)
+    videos = api_call(workout_id, next_page)
+    @workouts = pull_video_info(videos)
+  end
+  
 end
-
-
-
-# def show
-#   @workout_id = params[:id]
-#   # @next_page = params[:nextPageToken]
-#   retrieve_videos(@workout_id, "")
-# end
-
-# def update
-#   @next_page = params[:nextPageToken]
-#   puts @next_page
-#   @workout_id = params[:id]
-#   puts @workout_id
-#   retrieve_videos(@workout_id, @next_page)
-# end
-
-
-# private 
-
-# def retrieve_videos(workout_id, next_page)
-#   videos = api_call(workout_id, next_page)
-#   @workouts = pull_video_info(videos)
-# end
-# end
