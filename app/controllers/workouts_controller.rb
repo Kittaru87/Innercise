abea24a6e931633501d58b6b2263da4f953997e3
Sophia Bell <sophbell87@gmail.com>
2020-05-27 15:49:36 +0100

    button works - just need to check when have more quota

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -4,26 +4,38 @@ class WorkoutsController < ApplicationController
   include WorkoutsHelper
 
   def show
-  @workout_id = params[:id]
-  # @next_page = params[:nextPageToken]
-  @previous_page = params[:prevPageToken]
-  puts @previous_page
-  if @previous_page == nil
+    @workouts = session[:workouts]
+    @workout_id = params[:id]
+    puts 'start'
+    puts @workouts
+    if !@workouts.empty? 
+      @previous_page = @workouts[0]["prevPageToken"]
+      puts "previous page"
+      puts @previous_page
+      @workouts
+    else
     retrieve_videos(@workout_id, "")
-  else 
-    retrieve_videos(@workout_id, @next_page)
   end
   puts @workouts
   end
 
   def update
     @next_page = params[:nextPageToken]
-    puts @next_page 
-    # @workout_id = params[:id]
+    # puts @next_page 
+    @previous_page = params[:prevPageToken]
+    # puts @previous_page
+    @workout_id = params[:id]
     # puts @workout_id
-    # retrieve_videos(@workout_id, @next_page)
+    retrieve_videos(@workout_id, @next_page)
     # puts @workouts
+    session[:workouts] = @workouts
     redirect_to workout_path(@workout_id)
+  end
+
+  def destroy
+    @workouts = session[:workouts]
+    @workouts.destroy
+    redirect_to :root 
   end
 
 private 
