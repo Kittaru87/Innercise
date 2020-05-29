5bf4ece7919dbd359e205c4d6a7e34ef469be6e2
Sophia Bell <sophbell87@gmail.com>
2020-05-29 16:26:17 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,28 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_529_142_835) do
+ActiveRecord::Schema.define(version: 2020_05_29_142835) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'sessions', force: :cascade do |t|
-    t.string 'session_id', null: false
-    t.text 'data'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['session_id'], name: 'index_sessions_on_session_id', unique: true
-    t.index ['updated_at'], name: 'index_sessions_on_updated_at'
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
-  create_table 'workouts', force: :cascade do |t|
-    t.string 'bodyId'
-    t.string 'videoId'
-    t.string 'title'
-    t.string 'description'
-    t.string 'channel'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'nextPageToken'
-    t.string 'prevPageToken'
+  create_table "workouts", force: :cascade do |t|
+    t.string "bodyId"
+    t.string "videoId"
+    t.string "title"
+    t.string "description"
+    t.string "channel"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "nextPageToken"
+    t.string "prevPageToken"
   end
+
 end
