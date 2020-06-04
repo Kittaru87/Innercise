b42b93e8f653a9ccb87f6d6922051c2cac9fb6ca
Sophia Bell <sophbell87@gmail.com>
2020-06-04 15:57:19 +0100

    username now populating db

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class RegistrationsController < Devise::RegistrationsController
+
   private
 
   def sign_up_params
@@ -11,8 +12,4 @@ class RegistrationsController < Devise::RegistrationsController
     params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password)
   end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-  end
 end
