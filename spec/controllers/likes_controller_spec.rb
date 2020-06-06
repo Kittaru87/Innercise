09290c8021622fdd9d0243ddd8560eddfaa3561a
Sophia Bell <sophbell87@gmail.com>
2020-06-06 17:16:16 +0100

    fixing a bug when the video is broken

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -2,18 +2,20 @@
 
 require 'rails_helper'
 
-RSpec.describe RegistrationsController, type: :controller do
+RSpec.describe LikesController, type: :controller do
   before(:each) do
     @user = User.create { :user }
+    @workout = Workout.create { :workout }
+    @like = Like.create(user_id: @user.id, workout_id: @workout.id)
   end
-
-  # it 'should have a current_user' do
-  #   expect(subject.current_user).to_not eq(nil)
-  # end
 
   describe 'GET #index' do
     it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
+  end
+
+  it 'creates a like' do
+    expect(@like).to be_a_new(Like)
   end
 end
