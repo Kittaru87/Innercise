b93b257c5186729031a9ca8c960f7728b3d2df40
Sophia Bell <sophbell87@gmail.com>
2020-06-06 14:26:07 +0100

    adding likes table and model

---
+++ app/models/like.rb
@@ -0,0 +1,4 @@
+class Like < ApplicationRecord
+  belongs_to :workout
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_131745) do
+ActiveRecord::Schema.define(version: 2020_06_06_132523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "workout_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_likes_on_user_id"
+    t.index ["workout_id"], name: "index_likes_on_workout_id"
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.string "session_id", null: false
@@ -49,4 +58,6 @@ ActiveRecord::Schema.define(version: 2020_06_04_131745) do
     t.string "prevPageToken"
   end
 
+  add_foreign_key "likes", "users"
+  add_foreign_key "likes", "workouts"
 end
