6d75b9a681f05372984dcf6255750eb5f519b084
Sophia Bell <sophbell87@gmail.com>
2020-06-06 15:58:48 +0100

    can't like more a workout more than once, button doesn't change colour yet

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,23 +2,22 @@ class LikesController < ApplicationController
   before_action :find_workout
   
   def create
-    # if already_liked?
-    #   flash[:notice] = "You can't like more than once"
-    # else
+    if already_liked?
+      flash[:notice] = "You can't like a workout more than once"
+    else
       @workout = Like.create(user_id: current_user.id, workout_id: params[:workout_id])
-    # end
-    # redirect_to workout_path(@workout['bodyId'])
-    # redirect_to workout_path(@post)
+    end
+    redirect_to workout_path(@found_workout['bodyId'])
   end
 
   private
 
   def find_workout
-    Workout.find(params[:workout_id])
+    @found_workout = Workout.find(params[:workout_id])
   end
 
-  # def already_liked?
-  #   Like.where(user_id: current_user.id, workout_id: params[:workout_id]).exists?
-  # end
+  def already_liked?
+    Like.where(user_id: current_user.id, workout_id: params[:workout_id]).exists?
+  end
 
 end
