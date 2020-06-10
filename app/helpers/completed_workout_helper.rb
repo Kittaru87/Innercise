799c010d5561b544b452d76409b62db97803c5f8
Sophia Bell <sophbell87@gmail.com>
2020-06-10 17:44:42 +0100

    adding in methods for weekly, monthly and yearly workout completed

--- app/helpers/completed_workout_helper.rb
+++ app/helpers/completed_workout_helper.rb
@@ -4,4 +4,12 @@ module CompletedWorkoutHelper
     @all_time = CompletedWorkout.where(user_id: current_user.id).count
   end
 
+  def one_week
+    @one_week = CompletedWorkout.where(user_id: current_user.id).where('created_at >= ?', 1.week.ago).count
+  end
+
+  def one_month
+    @one_month = CompletedWorkout.where(user_id: current_user.id).where('created_at >= ?', 1.month.ago).count
+  end
+
 end
--- app/models/completed_workout.rb
+++ app/models/completed_workout.rb
@@ -2,7 +2,7 @@ class CompletedWorkout < ApplicationRecord
   belongs_to :workout
   belongs_to :user
 
-  scope :week, -> { select('current_user, count(user_id) as count').where('created_at > current_date - interval, 7 days').group(:user_id) }
+  scope :week, -> { select('current_user, count(user_id) as count').where("created_at > ?", 1.week.ago).group(:user_id) }
 
   # SELECT user_id, count(user_id) as count FROM "completed_workouts" WHERE created_at > current_date - interval '7 days' AND user_id = 5 GROUP BY user_id
 end
