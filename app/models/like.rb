f47a8b7f73c1d7c4acb73898d405d89d9223aad8
Sophia Bell <sophbell87@gmail.com>
2020-06-15 18:12:22 +0100

    inner join scope works for public liked workouts

--- app/models/like.rb
+++ app/models/like.rb
@@ -6,9 +6,8 @@ class Like < ApplicationRecord
 
   scope :popular, -> { select('workout_id, count(workout_id) as count').group(:workout_id).order('count desc').limit(5) }
 
-  scope :body_popular, -> { Like.select(:workout_id, :'workouts.bodyId', :'workouts.videoId', :'workouts.title').joins('workouts').where('likes.workout_id = workouts.id') }
-    
-  # Like.joins('workouts').select('likes.workout_id', 'workouts.bodyId', 'workouts.videoId', 'workouts.title').where('likes.workout_id = workouts.id') }
-
-  
+  scope :body_popular, -> { Workout.joins(:likes).
+                            select('likes.workout_id', 'bodyId', 'videoId', 'title') 
+                          }
+      
 end
