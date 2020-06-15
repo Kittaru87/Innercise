8e10d2532b6bd8676e0081806fb874e85586d771
Sophia Bell <sophbell87@gmail.com>
2020-06-15 18:40:35 +0100

    moving popular scope from like to workout

--- app/models/like.rb
+++ app/models/like.rb
@@ -5,10 +5,4 @@ class Like < ApplicationRecord
   belongs_to :user
 
   scope :popular, -> { select('workout_id, count(workout_id) as count').group(:workout_id).order('count desc').limit(5) }
-
-  scope :body_popular, lambda {
-                         Workout.joins(:likes)
-                                .select('likes.workout_id', 'bodyId', 'videoId', 'title')
-                                .distinct
-                       }
 end
